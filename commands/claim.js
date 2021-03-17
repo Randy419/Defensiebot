818188bf26a129e7532c1bd748d54e57812435dc
Randy419 <71568554+Randy419@users.noreply.github.com>
2021-03-17 16:16:33 +0100

    Update claim.js

--- commands/claim.js
+++ commands/claim.js
@@ -1,9 +1,8 @@
 const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-
-    return message.channel.send("Goedendag. ${message.author.username} Heeft het ticket geclaim als iemand anders hierin praat warn");
     if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Je hebt geen toestemming om dit te doen.");
+    return message.channel.send(`Goedendag. ${message.author.username} Heeft het ticket geclaim als iemand anders hierin praat warn`);
 }
 
 module.exports.help = {
