404abe5204c454c49b01d94a2a975542f9552d37
Randy419 <71568554+Randy419@users.noreply.github.com>
2021-03-18 17:23:19 +0100

    Update staff.js

--- commands/staff.js
+++ commands/staff.js
@@ -2,29 +2,29 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("ADMINISTRATOR");
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("ADMINISTRATOR");
 
-    var user = message.member.guild.member(message.mentions.user.first());
-    if(!user) return message.reply("Geen gebruiker gevonden");
+    var user = message.guild.member(message.mentions.user.first());
+    if (!user) return message.reply("Geen gebruiker gevonden");
 
-    var role = message.member.guild.roles.find(r => r.name ==="Staff team");
+    var role = message.guild.roles.cache.find(r => r.name ==="Staff team");
     if (!role) return message.reply("Geen rol met deze naam Gevonden");
 
-        var Foutembed = new discord.MessageEmbed()
+    var Foutembed = new discord.MessageEmbed()
         .setTitle("Deze Persoon is al admin")
         .setColor("ff0000")
         .setDescription("ik denk dat je een foutje hebt Gemaakt");
 
-        var embed = new discord.MessageEmbed()
+    var embed = new discord.MessageEmbed()
         .setTitle("****AanPassing****")
         .setColor("ff0000")
         .setDescription("Rol Aanpassing", `${user.user.username} heeft de rol ${role} Gekregen en is nu admin`);
 
-        if(user.roles.cache.find(r => r.name === role.name)){
-            return message.reply(Foutembed);
-        }
+    if (user.roles.cache.find(r => r.name === role.name)) {
+        return message.reply(Foutembed);
+    }
 
-        user.roles.add(role.id) && message.channel.send(embed);
+    user.roles.add(role.id) && message.channel.send(embed);
 }
 
 module.exports.help = {
